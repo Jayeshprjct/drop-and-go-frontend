3e377249188e5605c4d0aa5c5a85431ad612b89f
Aditya Singh <aditya@cleverstudio.in>
2023-04-28 01:25:54 +0530

    Updated Image path

--- src/Pages/Carousel.jsx
+++ src/Pages/Carousel.jsx
@@ -3,10 +3,10 @@ import styles from "../styles/CarouselC.module.css";
 
 const CarouselC = () => {
   const images = [
-    "/public/img1.jpeg",
-    "/public/img2.jpeg",
-    "/public/img3.jpeg",
-    "/public/img4.jpeg",
+    "src/assets/img1.jpeg",
+    "src/assets/img2.jpeg",
+    "src/assets/img3.jpeg",
+    "src/assets/img4.jpeg",
   ];
   const [currentSlide, setCurrentSlide] = useState(0);
 
